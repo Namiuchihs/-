2f608f582e1b93ea738176eb88573f10cf03e36b
Namiuchihs <sakuya0896@gmail.com>
2026-03-19 17:25:47 +0900

    Create nav.php

---
+++ templates/nav.php
@@ -0,0 +1,9 @@
+<nav>
+  <a href="../index.html">トップ</a> | 
+  <a href="../info.html">情報</a> | 
+  <a href="../rekishi.html">歴史</a> | 
+  <a href="../midokoro.html">見どころ</a> | 
+  <a href="../tos.html">利用規約</a> | 
+  <a href="../privacy.html">プライバシーポリシー</a> | 
+  <a href="videos.php" class="right-button">動画サイトへ</a>
+</nav>
